3ae0643b9582a9cc6eafaef82342096df6e73bef
ClaySamuelC <claysamuelc@gmail.com>
2020-08-18 14:01:27 -0700

    add reject function

--- src/underbar.js
+++ src/underbar.js
@@ -90,6 +90,13 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var results = [];
+    for (var i = 0; i < collection.length; i++) {
+      if (!test.call(null, collection[i])) {
+        results.push(collection[i]);
+      }
+    }
+    return results;
   };
 
   // Produce a duplicate-free version of the array.
